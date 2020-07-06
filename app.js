121f9feda56b8901795a85c2175bab619f75c04f
anapay <anapay42@gmail.com>
2020-07-06 19:29:55 -0300

    bootstrap

--- app.js
+++ app.js
@@ -5,20 +5,24 @@ const handlebars = require("express-handlebars")
 const bodyParser = require("body-parser")
 //const mongoose = require("mongoose")
 const app = express()
+const admin = require("./routes/admin")
+const path = require("path")
 //configurações
-  //body-parser
-  app.use(bodyParser.urlencoded({extended: true}))
-  app.use(bodyParser.json())
-  //handlebars
-  app.engine("handlebars", handlebars({defaultLayout: "main"}))
-  app.set("view engine", "handlebars")
-
+//body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+//handlebars
+app.engine("handlebars", handlebars({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+//public arquivos staticos
+app.use(express.static(path.join(__dirname, "public")))
 
 //rotas
+app.use("/admin",admin)
 
 
 //outros
 const PORT = 8989
-app.listen(PORT,()  =>{
+app.listen(PORT, () => {
     console.log("Server running!")
 })
