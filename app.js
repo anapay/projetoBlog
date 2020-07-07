94bd70cf3f2d80aa540c63a5ba8b3ad414dedbde
anapay <anapay42@gmail.com>
2020-07-07 14:30:42 -0300

    spacing

--- app.js
+++ app.js
@@ -3,17 +3,20 @@
 const express = require("express")
 const handlebars = require("express-handlebars")
 const bodyParser = require("body-parser")
-//const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 const app = express()
 const admin = require("./routes/admin")
 const path = require("path")
 //configurações
+
 //body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
 //handlebars
 app.engine("handlebars", handlebars({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
+
 //public arquivos staticos
 app.use(express.static(path.join(__dirname, "public")))
 
