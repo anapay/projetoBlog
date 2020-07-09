e6beaa80206388d30f252fff83e14f85179d2226
anapay <anapay42@gmail.com>
2020-07-08 23:56:49 -0300

    listing categories

--- routes/admin.js
+++ routes/admin.js
@@ -14,7 +14,11 @@ router.get("/posts", (req, res) => {
 })
 
 router.get("/categorias", (req, res) => {
-    res.render("admin/categorias")
+    Categoria.find().then((categorias))
+     res.render("admin/categorias", {categorias: categorias})
+}).catch((err) =>{
+     req.flash("Houve um erro ao listar as categorias!")
+     res.redirect("/admin")
 })
 
 router.get("/categorias/add", (req, res) => {
