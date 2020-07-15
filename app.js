e1503385e627137990a32a84f81dea41f8924f49
anapay <anapay42@gmail.com>
2020-07-15 18:38:10 -0300

    Exporting

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const path = require("path")
 const mongoose = require("mongoose")
 const session = require("express-session")
 const flash = require("connect-flash")
+require("./models/Postagem")
+const Postagem = mongoose.model("postagens")
+require("./models/Categoria")
+const Categoria = mongoose.model("categorias")
 //configurações
 //sessao
 app.use(session({
@@ -45,6 +49,73 @@ mongoose.connect("mongodb://localhost/blog").then(() => {
 app.use(express.static(path.join(__dirname, "public")))
 
 //rotas
+app.get("/", (req, res) => {
+
+    Postagem.find().lean().populate("categoria").sort({ data: "desc" }).then((postagens) => {
+        res.render("index", { postagens: postagens })
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno ao exibir os Posts!")
+        res.redirect("/404")
+    })
+
+})
+app.get("/postagem/:slug", (req, res) => {
+    Postagem.findOne({ slug: req.params.slug }).lean().then((postagem) => {
+        if (postagem) {
+            res.render("postagem/index", { postagem: postagem })
+
+        } else {
+            req.flash("error_msg", "Esse post não existe!")
+            res.redirect("/")
+
+        }
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/")
+    })
+})
+
+app.get("/categorias", (req, res) => {
+
+    Categoria.find().lean().then((categorias) => {
+        res.render("categorias/index", { categorias: categorias })
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno ao listar as Categorias!")
+        res.redirect("/")
+    })
+
+})
+app.get("/categorias/:slug", (req, res) => {
+    Categoria.findOne({ slug: req.params.slug }).lean().then((categoria) => {
+        if (categoria) {
+
+            Postagem.find({ categoria: categoria._id }).lean().then((postagens) => {
+
+                res.render("categorias/postagens", { postagens: postagens, categoria: categoria })
+
+            }).catch((err) => {
+                req.flash("error_msg", "Erro ao listar Posts!")
+                res.redirect("/")
+            })
+
+        } else {
+            req.flash("error_msg", "Está categoria não existe!")
+            res.redirect("/")
+
+        }
+
+    }).catch((err) => {
+        req.flash("error_msg", "houve um erro interno ao carregar o Post desta Categoria!")
+        res.redirect("/")
+    })
+})
+
+app.get("/404", (req, res) => {
+    res.send("Erro 404!")
+})
+
 app.use("/admin", admin)
 
 
