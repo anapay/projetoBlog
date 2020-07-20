de27be66d8affaa9f4c9719ce4388dc9b02bffc1
anapay <anapay42@gmail.com>
2020-07-20 16:23:57 -0300

    deploy

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 
 
 if (process.env.NODE_ENV == "production") {
-    module.exports = { mongoURI: "mongodb+srv//blogApp:joao@cluster0.zi8dk.mongodb.net/<dbname>?retryWrites=true&w=majority" }
+    module.exports = { mongoURI: "mongodb//blogApp:joao@cluster0.zi8dk.mongodb.net/<dbname>?retryWrites=true&w=majority" }
 
 } else {
     module.exports = { mongoURI: "mongodb://localhost/blog" }
